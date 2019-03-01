c885f3d3e4bbd7f66298df9726a0f1bae6b2674a
lidongyang <15650785172@163.com>
2019-03-01 17:48:22 +0800

    第二次更改

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 $(clickMe).on('click',function(){
-    if(popover.style.display === 'none'){
-        $(popover).show()
+   
+    if(popover.style.display == 'block'){
+        $(popover).hide()
         
-    }else if(popover.style.display ==='block'){
-       $(popover).hide()
+    }else{
+       $(popover).show()
     }
     $(document).one('click',function(){
         $(popover).hide()
-    })
-   
+    }) 
 })
 $(wrapper).on('click',false)
+
